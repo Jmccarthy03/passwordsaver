6260fcc2ecdb5f0068b04445f6859e1450bcc65e
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2024-08-28 10:55:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 	const nameInput = document.getElementById('uname').value;
 	const passInput = document.getElementById('pword').value;
 	if (passInput === "Joshu@27mac03" && nameInput === "Joshawesome3")  window.location.href = "home.html";
+			
+	if (passInput === "12345678910" && nameInput === "OP_Gamer23194")  window.location.href = "home.html";
 	}
