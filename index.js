62db79655441b33db9639921c0637c4ae80c4347
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2024-08-27 22:14:01 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+		function checkinfo() {
+	const nameInput = document.getElementById('uname').value;
+	const passInput = document.getElementById('pword').value;
+	if (passInput === "Joshu@27mac03" && nameInput === "Joshawesome3")  window.location.href = "home.html";
+	}
