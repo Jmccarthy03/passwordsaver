880a4a64a6404927aaab20a55db178e809b14c50
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2024-08-28 15:58:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 	const nameInput = document.getElementById('uname').value;
 	const passInput = document.getElementById('pword').value;
 	if (passInput === "Joshu@27mac03" && nameInput === "Joshawesome3")  window.location.href = "home.html";
-		else if (passInput === "12345678910" && nameInput === "OP_Gamer23194")  window.location.href = "home.html";
-        	//else if (passInput === "T@nn3r" && nameInput === "Tman")  window.location.href = "home.html";
+		
+	else if (passInput === "12345678910" && nameInput === "OP_Gamer23194")  window.location.href = "home.html";
+        	
