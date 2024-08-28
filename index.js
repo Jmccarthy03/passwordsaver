3ad436a6858b55c0743121c4a1c5bb080cc58b18
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2024-08-28 15:37:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 	if (passInput === "Joshu@27mac03" && nameInput === "Joshawesome3")  window.location.href = "home.html";
 			
 		else if (passInput === "12345678910" && nameInput === "OP_Gamer23194")  window.location.href = "home.html";
-	}
+        else if (passInput === "T@nn3r" && nameInput === "tman")  window.location.href = "home.html";
