e6e63d6c8da82be343722647d9e3d12a0ba6a18a
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2024-08-28 21:41:35 -0500

    Update Blackjack.js

--- Blackjack.js
+++ Blackjack.js
@@ -1,8 +1,27 @@
 function Hit1(){
-    var randomNumber = Math.floor(Math.random() * 1) + 1;
-    var displayText = (randomNumber === 1) ? "Ace" : randomNumber;
-    document.getElementById("card1").textContent = displayText;
+    var randomNumber = Math.floor(Math.random() * 13) + 1;
+    var card;
+    
+    switch(randomNumber) {
+        case 1:
+            card = "Ace";
+            break;
+        case 11:
+            card = "Jack";
+            break;
+        case 12:
+            card = "Queen";
+            break;
+        case 13:
+            card = "King";
+            break;
+        default:
+            card = randomNumber.toString();
+            break;
+    }
 
+    document.getElementById("card1").textContent = card;
 }
+
 
 function dturn(){}
