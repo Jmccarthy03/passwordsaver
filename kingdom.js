1b2ff39365d9aa5ccad062dc1784aaa2a95db101
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2024-09-24 09:33:53 -0500

    Update kingdom.js

--- kingdom.js
+++ kingdom.js
@@ -1,6 +1,6 @@
 function createkingdom() {
   const nameinput = document.getElementById("domname").value
-  document.getElementById("kname").innerHTML = "nameinput";
+  document.getElementById("kname").innerHTML = nameinput;
 
   
 }
