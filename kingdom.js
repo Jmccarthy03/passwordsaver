939d0385d77942213e88f568bcbe4cd659394216
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2024-09-25 10:56:06 -0500

    Update kingdom.js

--- kingdom.js
+++ kingdom.js
@@ -4,4 +4,10 @@ function createKingdom() {
   document.getElementById("domname").style.display = "none";
   document.getElementById("start").style.display = "none";
   document.getElementById("dname").style.display = "none";
+  firstQuestion()
+}
+
+functon firstQuestion() {
+ document.getElementById("question").innerHTML = "Would You Like To Tax The People?";
+  
 }
